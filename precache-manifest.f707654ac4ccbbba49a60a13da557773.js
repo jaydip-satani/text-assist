0c9296e52786c2abf03596ed8f8440b9e82ed473
jaydip2207 <162003897+jaydip2207@users.noreply.github.com>
2024-08-06 07:47:29 +0530

    Updates

---
+++ precache-manifest.f707654ac4ccbbba49a60a13da557773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffae59482d2baf361f947443e8e39b16",
+    "url": "/text-assist/index.html"
+  },
+  {
+    "revision": "824d3f9028ed553ecf2e",
+    "url": "/text-assist/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "890f71b477c6124bfe37",
+    "url": "/text-assist/static/js/2.5fcb2c72.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/text-assist/static/js/2.5fcb2c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f15c1a701cb3d8b7116",
+    "url": "/text-assist/static/js/3.eb89dfe3.chunk.js"
+  },
+  {
+    "revision": "824d3f9028ed553ecf2e",
+    "url": "/text-assist/static/js/main.11a2b944.chunk.js"
+  },
+  {
+    "revision": "bc14e372f6b621897a48",
+    "url": "/text-assist/static/js/runtime-main.691cd05c.js"
+  }
+]);
